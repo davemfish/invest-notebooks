263955fdb8fa060ec07e61578bee01b0d081ff24
davemfish <davemfish@gmail.com>
2025-07-02 10:48:01 -0400

    update sdr notebook

--- pollination_explore_nb.py
+++ pollination_explore_nb.py
@@ -7,6 +7,7 @@ app = marimo.App(width="medium", app_title="Pollination explore")
 @app.cell
 def _():
     import glob
+    import math
     import os
 
     import marimo as mo
@@ -14,13 +15,27 @@ def _():
     import geometamaker
     import matplotlib.pyplot as plt
     from natcap.invest import datastack
+    import numpy
     import pandas
-    return datastack, geopandas, glob, mo, os, pandas
+    import pygeoprocessing
+    return (
+        datastack,
+        geopandas,
+        glob,
+        math,
+        mo,
+        numpy,
+        os,
+        pandas,
+        plt,
+        pygeoprocessing,
+    )
 
 
 @app.cell
 def _():
-    workspace = 'C:/Users/dmf/projects/forum/pollination/sample'
+    # workspace = 'C:/Users/dmf/projects/forum/pollination/sample'
+    workspace = 'C:/Users/dmf/projects/forum/pollination/sample_no_summer_fix'
     farm_vector_path = 'farm_results.shp'
     return farm_vector_path, workspace
 
@@ -36,6 +51,8 @@ def _(datastack, glob, os, workspace):
     logfile_path = glob.glob(os.path.join(workspace, '*.txt'))[0]
     _, datastack_info = datastack.get_datastack_info(logfile_path)
     args_dict = datastack_info.args
+    args_dict
+    # datastack_info.invest_version
     return (args_dict,)
 
 
@@ -53,46 +70,207 @@ def _(guild_table, mo):
 
 
 @app.cell
+def _(args_dict, numpy, os, plt, pygeoprocessing, workspace):
+    lulc_tif = os.path.join(workspace, args_dict['landcover_raster_path'])
+    def plot_lulc(filepath):
+        nodata = pygeoprocessing.get_raster_info(filepath)['nodata']
+        array = pygeoprocessing.raster_to_numpy_array(filepath)
+        masked_array = numpy.where(array == nodata, numpy.nan, array)
+        return plt.imshow(masked_array, cmap='Dark2')
+    plot_lulc(lulc_tif)
+    return
+
+
+@app.cell
 def _(biophys_table, mo):
     mo.ui.table(biophys_table)
     return
 
 
-@app.cell
-def _(farm_vector_path, geopandas, os, workspace):
-    vector = geopandas.read_file(os.path.join(workspace, farm_vector_path))
-    return (vector,)
-
-
-@app.cell
-def _(mo, vector):
-    mo.ui.table(vector.drop(columns=['geometry']))
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Farm Results""")
     return
 
 
 @app.cell
-def _():
-    # fields = ["usle_tot", "sed_export", "sed_dep", "avoid_exp", "avoid_eros"]
-    # fig, [[ax1, ax2, ax3], [ax4, ax5, ax6]] = plt.subplots(2, 3, figsize=(15, 4))
-    # for ax, field in zip([ax1, ax2, ax3, ax4, ax5], fields):
-    #     ws_vector.plot(ax=ax, column=field, cmap="Greens", edgecolor='lightgray')
-    #     ax.set(title=f"{field}")
-    #     ax.set_axis_off()
-    # ws_vector.plot(ax=ax6, facecolor="none", edgecolor='lightgray')
-    # ws_vector.apply(lambda x: ax6.annotate(text=x['ws_id'], xy=x.geometry.centroid.coords[0], ha='center'), axis=1);
-    # ax6.set_axis_off()
-    # fig
+def _(farm_vector_path, geopandas, mo, os, workspace):
+    farm_vector = geopandas.read_file(os.path.join(workspace, farm_vector_path))
+    mo.ui.table(farm_vector.drop(columns=['geometry']))
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""### NoData Summary""")
+    mo.md(
+        r"""
+    ### Pollinator Supply, based on 
+    - available nesting sites in each cell,  
+    - the floral resources (i.e., food) in surrounding cells. Floral resources in cells near nesting sites are given more weight than distant cells, according to the species’ average foraging range.  
+    - and the relative abundance of that pollinator species.
+    """
+    )
+    return
+
+
+@app.cell
+def _(glob, numpy, os, plt, pygeoprocessing, workspace):
+    # Pollinator Supply
+    supply_tifs = glob.glob(os.path.join(workspace, 'pollinator_supply*.tif'))
+
+    def read_masked_array(filepath):
+        nodata = pygeoprocessing.get_raster_info(filepath)['nodata']
+        array = pygeoprocessing.raster_to_numpy_array(filepath)
+        masked_array = numpy.where(array == nodata, numpy.nan, array)
+        return masked_array
+
+    n_plots = len(supply_tifs)
+    fig, axes = plt.subplots(1, n_plots, figsize=(12, 4))
+    for ax, tif in zip(axes, supply_tifs):
+        arr = read_masked_array(tif)
+        ax.imshow(arr, cmap='viridis')
+        ax.set(title=f"{os.path.basename(tif).replace('pollinator_supply_', '')}")
+        ax.set_axis_off()
+    fig.suptitle('Pollinator Supply')
+    return (read_masked_array,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    ## Pollinator Abundance
+    Pollinator supply is an indicator of where pollinators originate from on the landscape. Pollinator abundance indicates where pollinators are active on the landscape. Pollinator abundance depends on,  
+
+    - the floral resources that attract pollinators to a cell  
+    - the supply of pollinators that can access that cell.
+    """
+    )
+    return
+
+
+@app.cell
+def _(math, os, plt, read_masked_array):
+    def plot_abundance(tif_list):
+        n_plots = len(tif_list)
+        n_cols = n_plots
+        n_rows = 1
+        if n_plots > 4:
+            n_cols = 4
+            n_rows = int(math.ceil(n_plots / n_cols))
+        fig, axes = plt.subplots(n_rows, n_cols, figsize=(n_cols*3, n_rows*3))
+        for ax, tif in zip(axes.flatten(), tif_list):
+            arr = read_masked_array(tif)
+            ax.imshow(arr, cmap='viridis')
+            ax.set(title=f"{os.path.basename(tif).replace('pollinator_abundance_', '')}")
+            ax.set_axis_off()
+        return fig
+    return (plot_abundance,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""### Species Abundance""")
+    return
+
+
+@app.cell
+def _(glob, os, plot_abundance, workspace):
+    # Pollinator Abundance (species)
+    abundance_tifs = glob.glob(os.path.join(workspace, 'pollinator_abundance*.tif'))
+    plot_abundance(abundance_tifs)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""### Total Abundance""")
     return
 
 
 @app.cell
 def _():
+    return
+
+
+@app.cell
+def _(glob, os, plot_abundance, workspace):
+    total_abundance_tifs = glob.glob(os.path.join(workspace, 'total_pollinator_abundance*.tif'))
+    plot_abundance(total_abundance_tifs)
+    return
+
+
+@app.cell
+def _(
+    farm_vector_path,
+    glob,
+    os,
+    plot_abundance,
+    pprint,
+    pygeoprocessing,
+    workspace,
+):
+    ## Farm Pollinator Abundance
+    farm_abundance_tifs = glob.glob(os.path.join(workspace, 'intermediate_outputs', 'farm_pollinator*.tif'))
+    farm_abundance_tifs.extend(glob.glob(os.path.join(workspace, 'farm_pollinator*.tif')))
+    plot_abundance(farm_abundance_tifs)
+    for fa_tif in farm_abundance_tifs:
+        print(f'{os.path.basename(fa_tif)}')
+        fa_stats = pygeoprocessing.zonal_statistics((fa_tif, 1), os.path.join(workspace, farm_vector_path))
+        pprint.pprint(fa_stats)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""### Yields""")
+    return
+
+
+@app.cell
+def _(farm_vector_path, glob, os, pygeoprocessing, workspace):
+    ## Yields
+    import pprint
+    yield_tifs = glob.glob(os.path.join(workspace, '*_pollinator_yield*.tif'))
+    # plot_abundance(yield_tifs)
+    for yield_tif in yield_tifs:
+        print(f'{os.path.basename(yield_tif)}')
+        stats = pygeoprocessing.zonal_statistics((yield_tif, 1), os.path.join(workspace, farm_vector_path))
+        pprint.pprint(stats)
+        # yield_array = read_masked_array(yield_tif)
+        # print(f'{os.path.basename(yield_tif)}: {numpy.nansum(yield_array)}')
+    return (pprint,)
+
+
+@app.cell
+def _(farm_vector_path, glob, os, pprint, pygeoprocessing, workspace):
+    # Managed Pollinator
+    managed_pollinator_tif = glob.glob(os.path.join(workspace, 'intermediate_outputs', 'managed_pollinators*.tif'))[0]
+    managed_stats = pygeoprocessing.zonal_statistics((managed_pollinator_tif, 1), os.path.join(workspace, farm_vector_path))
+    pprint.pprint(managed_stats)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    Wild pollinator yield on a farm = total pollinator yield - managed pollinator yield  
+    0 = 0.9 - 0.9  
+    Total pollinator yield = managed + farm abundance  
+    0.9 = 0.9 + 0
+
+    Why is farm abundance 0 on the first farm?
+
+    Farm abundance (spring) is a product of half_sat (spring) and total pollinator abundance
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r""" """)
     return
 
 
--- sdr_explore_nb.py
+++ sdr_explore_nb.py
@@ -1,3 +1,10 @@
+# /// script
+# dependencies = [
+#   geopandas,
+#   matplotlib
+# ]
+# ///
+
 import marimo
 
 __generated_with = "0.13.15"
@@ -12,21 +19,55 @@ def _():
     import geopandas
     import geometamaker
     from natcap.invest.sdr import sdr
+    from natcap.invest import datastack
+    import natcap.invest.utils
     import matplotlib.pyplot as plt
-    return geopandas, mo, os, plt
+    import pygeoprocessing
+    import yaml
+
+    import utils
+    return (
+        datastack,
+        geometamaker,
+        geopandas,
+        mo,
+        natcap,
+        os,
+        plt,
+        pygeoprocessing,
+        utils,
+        yaml,
+    )
 
 
 @app.cell
-def _():
-    workspace = 'C:/Users/dmf/projects/forum/sdr/sample_3_16_0'
-    watershed_results_vector = 'watershed_results_sdr_gura.shp'
-    return watershed_results_vector, workspace
+def _(datastack, mo):
+    logfile_path = mo.cli_args().get('logfile')
+    _, ds_info = datastack.get_datastack_info(logfile_path)
+    args_dict = ds_info.args
+    return (args_dict,)
+
+
+@app.cell
+def _(args_dict, natcap):
+    # workspace = 'C:/Users/dmf/projects/forum/sdr/sample_3_16_0'
+    # watershed_results_vector = 'watershed_results_sdr_gura.shp'
+    workspace = args_dict['workspace_dir']
+    suffix_str = natcap.invest.utils.make_suffix_string(args_dict, 'results_suffix')
+    watershed_results_vector = f'watershed_results_sdr{suffix_str}.shp'
+    return suffix_str, watershed_results_vector, workspace
 
 
 @app.cell
 def _(geopandas, os, watershed_results_vector, workspace):
     ws_vector = geopandas.read_file(os.path.join(workspace, watershed_results_vector))
     return (ws_vector,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Results by Watershed""")
+    return
 
 
 @app.cell
@@ -37,15 +78,58 @@ def _(mo, ws_vector):
 
 @app.cell
 def _(plt, ws_vector):
-    fields = ["usle_tot", "sed_export", "sed_dep", "avoid_exp", "avoid_eros"]
-    fig, [[ax1, ax2, ax3], [ax4, ax5, ax6]] = plt.subplots(2, 3, figsize=(15, 4))
-    for ax, field in zip([ax1, ax2, ax3, ax4, ax5], fields):
-        ws_vector.plot(ax=ax, column=field, cmap="Greens", edgecolor='lightgray')
-        ax.set(title=f"{field}")
-        ax.set_axis_off()
-    ws_vector.plot(ax=ax6, facecolor="none", edgecolor='lightgray')
-    ws_vector.apply(lambda x: ax6.annotate(text=x['ws_id'], xy=x.geometry.centroid.coords[0], ha='center'), axis=1);
-    ax6.set_axis_off()
+    def _plot():
+        fields = ["usle_tot", "sed_export", "sed_dep", "avoid_exp", "avoid_eros"]
+        fig, [[ax1, ax2, ax3], [ax4, ax5, ax6]] = plt.subplots(2, 3, figsize=(15, 4))
+        for ax, field in zip([ax1, ax2, ax3, ax4, ax5], fields):
+            ws_vector.plot(ax=ax, column=field, cmap="Greens", edgecolor='lightgray')
+            ax.set(title=f"{field}")
+            ax.set_axis_off()
+        ws_vector.plot(ax=ax6, facecolor="none", edgecolor='lightgray')
+        ws_vector.apply(lambda x: ax6.annotate(text=x['ws_id'], xy=x.geometry.centroid.coords[0], ha='center'), axis=1);
+        ax6.set_axis_off()
+        return fig
+    _plot()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Stream Network""")
+    return
+
+
+@app.cell
+def _(math, os, plt, pygeoprocessing, suffix_str, utils, workspace):
+    streams_raster_path = os.path.join(workspace, f'stream{suffix_str}.tif')
+    dem_raster_path = os.path.join(workspace, 'intermediate_outputs', f'pit_filled_dem{suffix_str}.tif')
+    drains_to_stream_path = os.path.join(workspace, 'intermediate_outputs', f'what_drains_to_stream{suffix_str}.tif')
+
+    raster_info = pygeoprocessing.get_raster_info(dem_raster_path)
+    bbox = raster_info['bounding_box']
+    xy_ratio = (bbox[2] - bbox[0]) / (bbox[3] - bbox[1])
+    print(xy_ratio)
+
+    def plot_raster_list(tif_list, colormap='viridis', row_major=True):
+        n_plots = len(tif_list)
+        n_cols = n_plots
+        n_rows = 1
+        if n_plots > 4:
+            n_cols = 4
+            n_rows = int(math.ceil(n_plots / n_cols))
+        if not row_major:
+            nc = n_cols
+            n_cols = n_rows
+            n_rows = nc
+        fig, axes = plt.subplots(n_rows, n_cols, figsize=(n_cols*xy_ratio*2, n_rows*2))
+        for ax, tif in zip(axes.flatten(), tif_list):
+            arr = utils.read_masked_array(tif)
+            ax.imshow(arr, cmap=colormap)
+            ax.set(title=f"{os.path.basename(tif)}")
+            ax.set_axis_off()
+        return fig
+
+    fig = plot_raster_list([streams_raster_path, dem_raster_path, drains_to_stream_path], row_major=False)
     fig
     return
 
@@ -53,6 +137,41 @@ def _(plt, ws_vector):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""### NoData Summary""")
+    return
+
+
+@app.cell
+def _(geometamaker, yaml):
+    def geometamaker_load(filepath):
+        with open(filepath, 'r') as file:
+            yaml_string = file.read()
+            yaml_dict = yaml.safe_load(yaml_string)
+            if not yaml_dict or ('metadata_version' not in yaml_dict
+                                 and 'geometamaker_version' not in yaml_dict):
+                message = (f'{filepath} exists but is not compatible with '
+                           f'geometamaker.')
+                raise ValueError(message)
+
+        return geometamaker.geometamaker.RESOURCE_MODELS[yaml_dict['type']](**yaml_dict)
+    return (geometamaker_load,)
+
+
+@app.cell
+def _(geometamaker, geometamaker_load, os, pandas, workspace):
+    raster_summary = {}
+    for path, dirs, files in os.walk(workspace):
+        for file in files:
+            if file.endswith('.yml'):
+                filepath = os.path.join(path, file)
+                try:
+                    resource = geometamaker_load(filepath)
+                except Exception as err:
+                    print(filepath)
+                    raise err
+                if isinstance(resource, geometamaker.models.RasterResource):
+                    band = resource.get_band_description(1)
+                    raster_summary[os.path.basename(resource.path)] = band.gdal_metadata
+    pandas.DataFrame(raster_summary)
     return
 
 
